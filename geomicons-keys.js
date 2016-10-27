4a6290bd0a45975616e8df1e00dd465276c8009c
Brent Jackson <jxnblk@gmail.com>
2016-10-26 21:18:27 -0400

    Add simple icons

--- geomicons-keys.js
+++ geomicons-keys.js
@@ -1,4 +1,13 @@
 
 const geomiconsPaths = require('geomicons-open')
+
+const blacklist = [
+  'twitter',
+  'facebook',
+  'github',
+  'dribbble'
+]
+
 module.exports = Object.keys(geomiconsPaths)
+  .filter(k => blacklist.indexOf(k) < 0)
 
