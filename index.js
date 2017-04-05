d615b9b8ff11fceb9a08ec08deed25298e036f37
Brent Jackson <jxnblk@gmail.com>
2017-04-05 12:07:51 -0400

    Edit readme

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 
 // todo
 // const { paths: reline } = require('reline')
+const Icon = require('./Icon')
 const geomicons = require('geomicons-open')
 const material = require('./material-design-icons')
 const simple = require('./simple-icons')
 
 module.exports = Object.assign({},
+  Icon,
   geomicons,
   material,
   simple
